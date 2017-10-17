b22ad502627c17bea08cce160584895034c74646
Jgarrepy17 <jgarrepy@pathfinderstudents.org>
2017-10-17 11:06:15 -0400

    Add files via upload
    complete

---
+++ 2-While.js
@@ -0,0 +1,21 @@
+/*1.1- Conditionals with Prompt */
+
+//Conditional where flavor = chocalate or vanilla
+//Prompt for flavor, then output "here's your"+ flavor
+//if strawberry alert 'all out of strawberry'
+//else alert no such flavor
+//Continue while flavor does not equal coffee
+while (flavor != 'coffee'){
+flavor = prompt('Please enter the kind of ice cream you want either vanilla chocalate or strawberry')
+
+if (flavor == 'chocalate' || flavor == 'strawberry'){
+	alert('you have selected '+flavor);
+	
+}
+else if (flavor == 'vanilla'){
+	alert('I am so sorry but all th e vanilla is gone');
+}
+else{
+	alert('No such flavor exists');
+}
+}
